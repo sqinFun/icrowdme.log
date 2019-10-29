9cb2580d2159020d6dd4e0a691ec059f1fe35de3
sqinFun <denchyg@yandex.ru>
2019-10-30 01:14:05 +0300

    Добавил главный сегмент

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,18 @@
 $(function(){
 
+    $('.main-section-slider').slick({
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        nextArrow: $('.main-section-slider-next'),
+        prevArrow: $('.main-section-slider-previous'),
+        autoplay:true,
+        autoplaySpeed: 5000,
+      });
 
 
-  
+
+
+
 });
